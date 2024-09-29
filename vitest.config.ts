82c9eefc798cf3ce16305935afffe7628b59e4a8
Peter Hirn <peter.hirn@phi.ag>
2024-09-29 12:34:01 +0200

    test: fix coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     },
     coverage: {
       all: true,
-      include: ["wasm/**"],
+      include: ["src/**"],
       reportsDirectory: "coverage",
       reporter: ["text", "cobertura", "html"]
     }
