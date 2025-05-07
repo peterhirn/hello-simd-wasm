798e0c7ee13211673075f37353991b0b3d6412af
Peter Hirn <peter.hirn@phi.ag>
2025-05-07 12:57:28 +0200

    chore: move pnpm settings

--- eslint.config.js
+++ eslint.config.js
@@ -13,9 +13,7 @@ export default tseslint.config(
   {
     files: ["**/*.js"],
     languageOptions: {
-      globals: {
-        ...globals.node
-      }
+      globals: globals.node
     }
   },
   {
