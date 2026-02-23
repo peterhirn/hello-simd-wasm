91ab59c5958cd1469a754005007850e2037acfe3
Peter Hirn <peter.hirn@phi.ag>
2026-02-23 11:53:36 +0100

    chore(lint): migrate config

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
 // @ts-check
 import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
+export default defineConfig(
   {
     ignores: ["wasm/**", "dist/**", "target/**"]
   },
@@ -13,7 +14,7 @@ export default tseslint.config(
   {
     files: ["**/*.js"],
     languageOptions: {
-      globals: globals.node
+      globals: globals.nodeBuiltin
     }
   },
   {
