ec62b29b190a2e07450eb476b80d63b63f85ff2a
Peter Hirn <peter.hirn@phi.ag>
2026-02-26 08:46:49 +0100

    chore: upgrade to rust 2024 edition

--- wasm/lib.rs
+++ wasm/lib.rs
@@ -2,31 +2,31 @@
 use std::{alloc::Layout, simd::f32x4};
 
 /// # Safety
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub unsafe fn alloc(size: u32) -> *mut u8 {
     let layout = Layout::array::<u8>(size as usize).unwrap();
-    std::alloc::alloc(layout)
+    unsafe { std::alloc::alloc(layout) }
 }
 
 /// # Safety
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub unsafe fn drop(ptr: *mut u32) {
-    let _ = Box::from_raw(ptr);
+    let _ = unsafe { Box::from_raw(ptr) };
 }
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub fn simd(input: f32) -> *mut [f32; 4] {
     let a = f32x4::splat(input);
     let b = f32x4::from_array([1.0, 2.0, 3.0, 4.0]);
     Box::into_raw(Box::new((a + b).to_array()))
 }
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub fn multivalue(input: u32) -> (u32, u32) {
     (input, input + 1)
 }
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub fn flip(a: u32, b: u32) -> (u32, u32) {
     (b, a)
 }
